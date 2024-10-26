a9444cc09794a1b36998d83ad7eb45756303d1ed
Miillla <p.milana0510@gmail.com>
2024-10-26 21:44:51 +0300

    hw20

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  const lists = $("#todoList");
+  const todos = JSON.parse(localStorage.getItem("todo-items")) || [];
+
+  todos.forEach((todo) => {
+    lists.append(`<p>${todo.text}</p>`);
+  });
+  $("#add-todo").on("click", function () {
+    const inputValue = $("#todo-input").val();
+
+    todos.push({ text: inputValue, completed: false });
+    localStorage.setItem("todo-items", JSON.stringify(todos));
+
+    lists.append(`<p>${inputValue}</p>`);
+    $("#todo-input").val("");
+  });
+  console.log("ready!");
+});
